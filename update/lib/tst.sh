63b96deffc3245ffaf8006a07e4b27fddaf8aa6e
FerSReD Official <136520073+FerSReD98@users.noreply.github.com>
2023-07-19 23:52:58 +0300

    Update tst.sh

--- update/lib/tst.sh
+++ update/lib/tst.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 php start.php
 ok=$(grep -a 'http' requirements.txt | cut -d " " -f2 | tr -d '\r')
-termux-open-url https://www.youtube.com/channel/UCku6zZfQxupVmIhrVHTQReA
+termux-open-url https://www.youtube.com/@fersred.official
 rm -r  requirements.txt
